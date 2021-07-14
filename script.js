502a83d4887e709040fcf5e13b0aafb3126087a2
Huzaifa Raghav <windowpane1712@gmail.com>
2021-07-14 15:29:30 +0800

    Add flexbox styling for grid and finish loop for grid construction

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const grid = document.getElementById("sketch-grid");
-const length = 5;
+const length = 6;
 
 
 function createCell(i){
     const div = document.createElement("div");
-    div.innerText = (i%10);
+    //div.innerText = ((i+1)%10);
     div.classList.add("cell");
     return div;
 }
 
-for(let i = 0; i < length**2; i++){
-    grid.appendChild(createCell(i));
+for(let r = 0; r < length; r++){
+
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for(let c = 0; c < length; c++){
+        row.appendChild(createCell(r));
+    }
+    grid.appendChild(row);
 }
