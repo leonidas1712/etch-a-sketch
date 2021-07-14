91d0c1faa79baa87fa5b882e64aea32bb94803b7
Huzaifa Raghav <windowpane1712@gmail.com>
2021-07-14 16:34:25 +0800

    Change to stopDrawing with Space click, disable using pointer-events on cell

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const grid = document.getElementById("sketch-grid");
 const length = 64;
 
-// key to hold to temporarily disable drawing
-const HOLD_KEY = "Space";
+// key to press to temporarily disable drawing
+const STOP_KEY = "Space";
+
 
 // mouseover event listener
 function cellHover(evt){
@@ -30,29 +31,69 @@ function createCell(i){
     return div;
 }
 
-for(let r = 0; r < length; r++){
-    const row = document.createElement("div");
-    row.classList.add("row");
+function gridInit(length){
+    for(let r = 0; r < length; r++){
+        const row = document.createElement("div");
+        row.classList.add("row");
 
-    for(let c = 0; c < length; c++){
-        row.appendChild(createCell(r));
+        for(let c = 0; c < length; c++){
+            row.appendChild(createCell(r));
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
+
+    addHover();
 }
 
-let removed = false;
-document.body.addEventListener('keydown', function(e){
-    if (e.code === HOLD_KEY && !removed){
-        removeHover();
-        removed = true;
+// functions to enable/disable hover(mouseover) for cells
+function hoverToggle(enable){
+    const cells = document.querySelectorAll(".cell");
+    if(enable){
+        cells.forEach((cell)=>{
+            cell.style.pointerEvents = "";
+        });
     }
-});
 
-document.body.addEventListener('keyup', function(e){
-    if(e.code == HOLD_KEY && removed){
-        addHover();
-        removed = false;
+    else{
+        cells.forEach((cell) => {
+            cell.style.pointerEvents = "none";
+        });
     }
-});
+}
 
-addHover();
+// Everytime keydown is activated the mouse pointer flickers even without hoverToggle, not sure why
+// when stop key is pressed, cells stop being colored. press again to enable drawing
+function stopDrawing(){
+    let removed = false;
+    document.body.addEventListener('keydown', function(e){
+        if(e.code == STOP_KEY && !removed){
+            hoverToggle(false);
+            removed = true;
+        }
+
+        else{
+            hoverToggle(true);
+            removed = false;
+        }
+    });
+}
+
+
+
+gridInit(length);
+stopDrawing();
+
+// let removed = false;
+//     document.body.addEventListener('keydown', function(e){
+//         if (e.code === HOLD_KEY && !removed){
+//             removeHover();
+//             removed = true;
+//         }
+//     });
+
+//     document.body.addEventListener('keyup', function(e){
+//         if(e.code == HOLD_KEY && removed){
+//             addHover();
+//             removed = false;
+//         }
+//     });
