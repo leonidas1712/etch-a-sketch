68b779ab5c23b29d8ae09a92018d2189d0d2e289
Huzaifa Raghav <windowpane1712@gmail.com>
2021-07-14 15:55:05 +0800

    Add hold to disable drawing feature

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 const grid = document.getElementById("sketch-grid");
-const length = 6;
+const length = 64;
 
+// key to hold to temporarily disable drawing
+const HOLD_KEY = "Space";
+
+// mouseover event listener
+function cellHover(evt){
+    this.classList.add('cell-hover');
+}
+
+// add hover listener to all the cells
+function addHover(){
+    document.querySelectorAll(".cell").forEach(cell => {
+        cell.addEventListener('mouseover', cellHover);
+    });
+}
+
+function removeHover(){
+    document.querySelectorAll(".cell").forEach(cell => {
+        cell.removeEventListener('mouseover', cellHover);
+    });
+}
 
 function createCell(i){
     const div = document.createElement("div");
     //div.innerText = ((i+1)%10);
     div.classList.add("cell");
+    //div.addEventListener('mouseover', cellHover);
     return div;
 }
 
 for(let r = 0; r < length; r++){
-
     const row = document.createElement("div");
     row.classList.add("row");
 
@@ -19,3 +39,20 @@ for(let r = 0; r < length; r++){
     }
     grid.appendChild(row);
 }
+
+let removed = false;
+document.body.addEventListener('keydown', function(e){
+    if (e.code === HOLD_KEY && !removed){
+        removeHover();
+        removed = true;
+    }
+});
+
+document.body.addEventListener('keyup', function(e){
+    if(e.code == HOLD_KEY && removed){
+        addHover();
+        removed = false;
+    }
+});
+
+addHover();
